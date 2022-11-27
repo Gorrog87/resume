d463db5cd234ff3209826dbb6ed0712643b6b479
Gorrog <gourguesmaxime@gmail.com>
2022-11-27 11:51:41 +0100

    resume page added

--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import Header from "../components/Header";
+import Bio from "../components/Bio";
+import Formation from "../components/Formation";
+import Skills from "../components/Skills";
+import Languages from "../components/Languagues";
+import Experiences from "../components/Experiences";
+
 
 export default function Resume(){
     return(
         <>
             <Header />
+            <Bio />
+            <Formation />
+            <Skills />
+            <Languages />
+            <Experiences />
+            <p><Link href="/">Back to the homepage</Link></p>
         </>
     );
 }
