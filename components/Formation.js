f60011ebe78a99995c69a7c9218814543de51906
Gorrog <gourguesmaxime@gmail.com>
2022-11-28 17:41:45 +0100

    resolved the fit content problem with the <a> tag in the formation component

--- components/Formation.js
+++ components/Formation.js
@@ -2,31 +2,34 @@ export default function Formation() {
   return (
     <>
       <div className="mb-10">
-        <h3 className="text-center my-20 underline underline-offset-8 "><strong>Formations</strong></h3>
-        <div className="text-center">
+        <h3 className="text-center my-20 underline underline-offset-8 ">
+          <strong>Formations</strong>
+        </h3>
+        <div className="flex justify-center">
           <a
             href="https://openclassrooms.com/fr/"
             target="_blank"
             rel="noreferrer"
-            className="flex justify-center"
+            className=""
           >
-            <img src="https://www.anaf.fr/wp-content/uploads/2020/09/OpenClassroom_LOGO.png"
+            <img
+              src="https://www.anaf.fr/wp-content/uploads/2020/09/OpenClassroom_LOGO.png"
               alt="openclassrooms-logo"
               className="w-32 mb-5"
-              id="logo"></img> 
-              
+              id="logo"
+            ></img>
           </a>
-          <p>
-             en alternance :
-            Concepteur / Développeur d'applications web JavaScript / ReactJs
-          </p>
         </div>
-        <div className="">
+        <p className="text-center">
+          en alternance : Concepteur / Développeur d'applications web JavaScript
+          / ReactJs
+        </p>
+        <div className="flex justify-center">
           <a
             href="https://www.lewagon.com/fr"
             target="_blank"
             rel="noreferrer"
-            className="flex justify-center"
+            className=""
           >
             <img
               src="https://www.frenchtechbordeaux.com/wp-content/uploads/2019/01/le-wagon-color.png"
@@ -35,11 +38,11 @@ export default function Formation() {
               id="logo"
             />
           </a>
-          <p className="text-center">
-            Paris (janvier - mars 2022) :
-            Concepteur / Développeur d'applications web
-          </p>
         </div>
+        <p className="text-center">
+          Paris (janvier - mars 2022) : Concepteur / Développeur d'applications
+          web
+        </p>
       </div>
     </>
   );
