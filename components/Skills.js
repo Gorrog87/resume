c55dd7433d2478c4d5d00fb9f532cbd5b2878edc
Gorrog <maximegourgues@protonmail.com>
2023-03-05 12:07:16 +0100

    switched text to logo in skills component

--- components/Skills.js
+++ components/Skills.js
@@ -1,112 +1,61 @@
-
 export default function Skills() {
   return (
     <>
-      <div className="md:flex md:justify-evenly">
+      <h2 className="text-center ">Hard Skills</h2>
+      <div className="flex md:justify-center">
+        <img src="./images/html.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+        <img src="./images/css3.png" alt="html-icon" className="code-icon"/>
+        <img src="./images/bootstrap-framework-logo.png" alt="html-icon" className="code-icon"/>
+        <img src="./images/tailwind-css.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+      <img src="./images/javascript-logo.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+        <img src="./images/react-native-logo.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+      <img src="./images/nextjs_icon_132160.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+        <img src="./images/ruby-logo.png" alt="html-icon" className="code-icon"/>
+      </div>
+      <div className="flex md:justify-center">
+        <img src="./images/server.png" alt="html-icon" className="code-icon"/>
+      </div>
+
+      <div>
+        <h3 className="my-20 underline underline-offset-8 text-center">
+          <strong>Soft Skills</strong>
+        </h3>
         <div className="text-center mb-10">
-          <h3 className="my-20 underline underline-offset-8 text-center"><strong>Hard Skills</strong></h3>
-
+          <div className="">
+            {/* <FontAwesomeIcon icon="fas fa-bullseye" className="iconHover" /> */}
+            <p>Cibler les demandes client</p>
+          </div>
           <div className="">
             {/* <FontAwesomeIcon
-            icon="fa-brands fa-html5"
-            id="html"
-            className="iconHover"
-          /> */}
-            <p className="">HTML</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-brands fa-css3-alt"
-              id="css"
-              className="iconHover"
-            /> */}
-            <p>CSS</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-brands fa-js-square"
-              id="js"
-              className="iconHover"
-            /> */}
-            <p>JavaScript</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-solid fa-gem"
-              id="ruby"
-              className="iconHover"
-            /> */}
-            <p>Ruby / Rails</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-brands fa-react"
-              id="react"
-              className="iconHover"
-            /> */}
-            <p>React</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-brands fa-git-square"
-              id="git"
-              className=""
-            /> */}
-            <p>Git / Github</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-solid fa-database"
-              id="sql"
-              className=""
-            /> */}
-            <p>SQL</p>
-          </div>
-
-          <div className="">
-            {/* <FontAwesomeIcon
-              icon="fa-solid fa-server"
-              id="heroku"
-              className=""
-            /> */}
-            <p>Heroku</p>
-          </div>
-        </div>
-        <div>
-          <h3 className="my-20 underline underline-offset-8 text-center"><strong>Soft Skills</strong></h3>
-          <div className="text-center mb-10">
-            <div className="">
-              {/* <FontAwesomeIcon icon="fas fa-bullseye" className="iconHover" /> */}
-              <p>Cibler les demandes client</p>
-            </div>
-            <div className="">
-              {/* <FontAwesomeIcon
                 icon="fa-solid fa-palette"
                 className="iconHover"
               /> */}
-              <p>UI / UX</p>
-            </div>
-            <div className="">
-              {/* <FontAwesomeIcon icon="fas fa-handshake" className="iconHover" /> */}
-              <p>Resolution des conflits</p>
-            </div>
-            <div className="">
-              {/* <FontAwesomeIcon icon="fas fa-comments" className="iconHover" /> */}
-              <p>Bonne communication</p>
-            </div>
-            <div className="">
-              {/* <FontAwesomeIcon
+            <p>UI / UX</p>
+          </div>
+          <div className="">
+            {/* <FontAwesomeIcon icon="fas fa-handshake" className="iconHover" /> */}
+            <p>Resolution des conflits</p>
+          </div>
+          <div className="">
+            {/* <FontAwesomeIcon icon="fas fa-comments" className="iconHover" /> */}
+            <p>Bonne communication</p>
+          </div>
+          <div className="">
+            {/* <FontAwesomeIcon
                 icon="fas fa-assistive-listening-systems"
                 className="iconHover"
               /> */}
-              <p>Empathique</p>
-            </div>
+            <p>Empathique</p>
           </div>
         </div>
       </div>
