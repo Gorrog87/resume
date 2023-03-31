e46044daabda42b012b4cb44bd72e42cb838492f
Gorrog <maximegourgues@protonmail.com>
2023-03-31 10:33:11 +0200

    commited the logo skills section, they ar way to heavy, i willfind some images lighter for the next time

--- components/Skills.js
+++ components/Skills.js
@@ -5,65 +5,65 @@ export default function Skills() {
 
       <div className="hard-skills">
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/html-logo.png"
             alt="html-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/css-logo.png"
             alt="css-icon"
             className="code-icon"
-          />
-          <img
+          /> */}
+          {/* <img
             src="./images/bootstrap-framework-logo.png"
             alt="bootsrtap-icon"
             className="code-icon"
-          />
-          <img
+          /> */}
+          {/* <img
             src="./images/tailwind-css.png"
             alt="tailwind-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/javascript-logo.png"
             alt="js-icon"
             className="code-icon"
-          />
-          <img
+          /> */}
+          {/* <img
             src="./images/react-native-logo.png"
             alt="react-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center"></div>
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/nextjs_icon_132160.png"
             alt="nextjs-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/ruby-logo.png"
             alt="ruby-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center">
-          <img
+          {/* <img
             src="./images/github.png"
             alt="github-icon"
             className="code-icon"
-          />
+          /> */}
         </div>
         <div className="flex justify-center">
-          <img src="./images/server.png" alt="db-icon" className="code-icon" />
+          {/* <img src="./images/server.png" alt="db-icon" className="code-icon" /> */}
         </div>
       </div>
 
